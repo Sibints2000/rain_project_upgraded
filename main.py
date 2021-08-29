48a1ca913990713ddab624fd120c8d9adb8dcdf9
Sibints2000 <sibints2000@gmail.com>
2021-08-29 12:04:43 +0530

    Fifth Commit

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if will_rain:
     client = Client(account_sid, auth_token)
     message = client.messages \
         .create(
-        body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-        from_='+15017122661',
-        to='+15558675310'
+        body="It's going to rain today. Remember to bring an umbrella.",
+        from_="+16318306012",
+        to="+917760279160"
     )
